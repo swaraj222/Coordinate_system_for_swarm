80cb282fa85109a3002d80eacc5f8076e9eb5738
swaraj222 <swaraj.nistane@gmail.com>
2024-01-24 19:43:21 +0530

    24th Jan

--- Algorithm_1.py
+++ Algorithm_1.py
@@ -2,16 +2,16 @@ import numpy as np
 import math
 import time
 import matplotlib.pyplot as plt
-from VST_generator import VST, num_robots
+from VST_generator import VST, num_robots, X, Y
 
-R = 4
+R = 4  # Initial radius to check for neighbours
 
 def neighbour_count(VST, id, R):
     count = 0
-    j = VST[id]
+    j = VST[f'{id}_dist']
     for i in j:
         if i < R:
-            count+=1
+            count += 1
     return count
 
 # print("Neighbour count", neighbour_count(VST, 4, R))
@@ -67,7 +67,8 @@ while l < num_robots:
         leader_id = l
     l += 1
 
-robots_list = sorted(VST[leader_id], reverse=True)
+VST['Leader_id']= leader_id
+robots_list = sorted(VST[f'{leader_id}_dist'])
 
 print(robots_list)
 
@@ -75,37 +76,73 @@ print(robots_list)
 
 def ref_select(VST, leader_id, robots_list):
     ref_robot = True
-    while ref_robot == True:
+    while ref_robot:
         ref_rob_a_id = 0
         ref_rob_b_id = 1
         a_dist = robots_list[ref_rob_a_id]
         b_dist = robots_list[ref_rob_b_id]
-        a_indent = np.where(VST[leader_id] == a_dist)
-        b_indent = np.where(VST[leader_id] == b_dist)
-
+        if a_dist == b_dist:
+            ref_rob_b_id +=1
+        a_indent = VST[f'{leader_id}_dist'].index(a_dist)
+        b_indent = VST[f'{leader_id}_dist'].index(b_dist)
+        if a_dist == b_dist:
+            b_indent +=1
         print(a_indent)
         print(b_indent)
 
-        if a_indent > leader_id:
+        if a_indent >= leader_id:
             a_indent += 1
-        if b_indent > leader_bid:
+        if b_indent >= leader_bid:
             b_indent += 1
-        ab_dist = VST[a_indent][b_indent]
+        ab_dist = VST[f'{a_indent}_dist'][b_indent]
         
-        print(ref_rob_a_id)
-        print(ref_rob_b_id)
+        print(a_indent)
+        print(b_indent)
 
         if ab_dist ==   a_dist + b_dist:   
             ref_robot = True
             ref_rob_b_id += 1
+            robots_list.pop(1)
+            print("In a line")
         else:
             ref_robot = False
-            # return ref_rob_a_id, ref_rob_b_id
+            return a_indent, b_indent
 
 # ref_rob_a_id, ref_rob_b_id = ref_select(VST, leader_id, robots_list)
-ref_select(VST, leader_id, robots_list)
-# print(ref_rob_a_id)
-# print(ref_rob_b_id)
-print(leader_id)
+ref_rob_a_id, ref_rob_b_id = ref_select(VST, leader_id, robots_list)
+
+VST['Reference Robot a'] = ref_rob_a_id
+VST['Reference Robot b'] = ref_rob_b_id
+
+print(ref_rob_a_id)
+print(ref_rob_b_id)
+print("Leader ID = ", leader_id)
+
+
+print(VST)
+
+
+
+
+plt.figure(2)
+plt.scatter(X,Y)
+plt.scatter(X[ref_rob_a_id],Y[ref_rob_a_id],c='orange')
+plt.annotate("Reference robot a", # this is the text
+                 (X[ref_rob_a_id],Y[ref_rob_a_id]), # these are the coordinates to position the label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center')
+plt.scatter(X[ref_rob_b_id],Y[ref_rob_b_id],c='orange',label="Reference robot b")
+plt.annotate("Reference robot b", # this is the text
+                 (X[ref_rob_b_id],Y[ref_rob_b_id]), # these are the coordinates to position the label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center')
+plt.scatter(X[leader_id],Y[leader_id],c='red',label="Leader robot")
+plt.annotate("Leader robot", # this is the text
+                 (X[leader_id],Y[leader_id]), # these are the coordinates to position the label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center')
 plt.show()
 
--- VST_generator.py
+++ VST_generator.py
@@ -23,20 +23,20 @@ import random
 # # Example Usage
 # if __name__ == "__main__":
 num_robots = 10
-VST = np.zeros((num_robots, num_robots)) # (Distance from each robot, number of data due to robots)
-
+VST_list = np.zeros((num_robots, num_robots)) # (Distance from each robot, number of data due to robots)
+VST = {}
 # print(VST)
 
 X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-Y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Y = [1, 2, 3, 5, 5, 6, 7, 8, 9, 10]
 
 # X = [4.5608, 2.5501, 4.0817, 1.3471, 9.7813, 5.5381, 4.5892, 8.5662, 6.0688, 4.5492]
 # Y = [8.0918, 7.4495, 5.2037, 0.8356, 7.521, 0.0171, 2.4966, 4.6773, 4.6039, 0.4696]
 
-for i in range(len(X)):
-    X[i] = random.uniform(0 , 10)
-for i in range(len(Y)):
-    Y[i] = random.uniform(0 , 10)
+# for i in range(len(X)):
+#     X[i] = random.uniform(0 , 10)
+# for i in range(len(Y)):
+#     Y[i] = random.uniform(0 , 10)
 
 X = list(np.around(X,4))
 Y = list(np.around(Y,4))
@@ -45,19 +45,30 @@ Y = list(np.around(Y,4))
 for i in range(num_robots):
     for j in range(num_robots):
         if j!=i:
-            VST[i][j] = (np.sqrt((Y[i]-Y[j])**2 + (X[i]- X[j])**2))
+            VST_list[i][j] = (np.sqrt((Y[i]-Y[j])**2 + (X[i]- X[j])**2))
+
+
 
 VST_i= []                         
 # np.array(VST_i)
-for k in VST:
+for k in VST_list:
     for l in k:
         if l == 0.0:
             k = np.delete(k, np.where(k == 0.0))
             k = k.tolist()
             VST_i.append(k)
 
-VST = VST_i 
+# VST_i = list(np.around(np.array(VST_i), 4))
+# VST_i = [ '%.4f' % elem for elem in VST_i ]
+
+for i in range(len(VST_list)):
+    VST_i[i] = [ round(elem, 4) for elem in VST_i[i] ]
+    VST[f'{i}_dist'] = VST_i[i]
+# print(VST)
+# print(VST_list)
 
 print("X", X)
 print("Y", Y)
+
+plt.figure(1)
 plt.scatter(X,Y)
